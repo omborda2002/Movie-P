38c3dbc9f3a3433a62f40ad0c63430b4842c60b3
Om_Borda <omborda2002@gmail.com>
2021-11-12 15:10:53 +0530

    all Fine that's time to go add js with express.

--- MDB with link/app.js
+++
@@ -1,8 +0,0 @@
-function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
-  
-  document.querySelector('.toggle')
-    .addEventListener('click', classToggle);
