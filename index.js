6107552533e45ec20abab54cb5f6cd6ce6237608
Om_Borda <omborda2002@gmail.com>
2021-11-16 23:29:14 +0530

    Search ok

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const data = require("./data.json");
 let datam;
-// TODO: JSON DATA 
+let dataSearch;
+// TODO: JSON DATA
 // fs.readFile("./data.json", "utf8", (err, jsonString) => {
 //   if (err) {
 //     console.log("Error reading file from disk:", err);
@@ -18,48 +19,51 @@ let datam;
 //   }
 // });
 
-
-
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-
-
 app.get("/", (req, res) => {
-  let num=0;
-  res.render("home",{data,num});
+  let num = 0;
+  res.render("home", { data, num });
 });
 
 app.get("/inner/:name", (req, res) => {
-  const {name}=req.params;
+  const { name } = req.params;
 
-  for(let i=0;i<data.length;i++){
-
-    if(data[i].name==name){
-      datam=data[i];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name == name) {
+      datam = data[i];
       break;
     }
-
   }
 
-  res.render("inner",{datam});
+  res.render("inner", { datam });
   // res.render("inner",{datam:data[name.valueOf()]});
 });
 
-app.get("/page/:num",(req,res)=>{
-  const {num}=req.params;
+app.get("/page/:num", (req, res) => {
+  const { num } = req.params;
   console.log(num);
-  res.render("home",{num,data});
-
+  res.render("home", { num, data });
 });
+
+//TODO: SEARCH :
 app.get("/search", (req, res) => {
   console.log(req.query);
-  res.send("Ok Search !");
+  const { q } = req.query;
+  console.log(q)
+  //search value :
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name == q) {
+      dataSearch = data[i];
+    }
+  }
+  console.log(dataSearch);
+  res.render("s", {dataSearch});
 });
 
 app.listen(3000, () => {
   console.log("Example app listening at http://localhost:3000");
 });
-
